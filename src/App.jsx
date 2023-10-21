23011a2c962608c56818c54f2e83dada05cd806e
Mary Queen Casaclang <108616687+queenyxies@users.noreply.github.com>
2023-10-21 12:14:38 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mary Queen Casaclang",
+  title: "IT Student",
+  email: "MaryQueen.Casaclang@studentambassadors.com",
+  gitHub: "queenyxies",
+  instagram: "queenyxies",
+  linkedIn: "queenyxies",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "queenyxies",
+  youTube: "maryqueencasaclang",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer_code.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Purrfect Code",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "An educational RPG game about Java programming made through Unity",
+    url: "https://github.com/queenyxies/Purrfect-Code",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Payroll System with Database",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "https://github.com/queenyxies/Payroll-System-with-Database",
   },
   {
-    title: "My Resume Site",
+    title: "Simple PingPong Game",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A simple ping pong game project made through Unity",
+    url: "https://github.com/queenyxies/SimplePingPong",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Weather Web App",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A React weather app for real-time updates on temperature, humidity, and wind speed using OpenWeatherMap API.",
+    url: "https://github.com/queenyxies/react-Weather-WebApp",
   },
 ];
 
